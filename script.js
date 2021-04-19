fb264de72ec61983763f166e829eb63a94a5591c
David Nuñez <nundavid99@gmail.com>
2021-04-19 18:59:49 -0400

    Implementacion de estructura p/ AFN

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+import { AFN, Estado, Transicion } from "./AFN.js";
+/*
 function conversionAFNaAFD(){
     //iniciar dEstados con cerradura(s0)
     dEstados =  cerradura(s0);
@@ -13,3 +14,32 @@ function conversionAFNaAFD(){
         }
     }
 }
+*/
+function hola(){
+    console.log('hola')
+}
+document.getElementById('regexInputButton').addEventListener('click', hola);
+
+(() => {
+    const regex = "(a|b)*abb";
+    
+    const estadoInicial = new Estado('s0');
+    const afn = new AFN(estadoInicial);
+
+    for( let i = 0; i < regex.length; i++ ){
+        const car = regex[i];
+        if("|()*+".contains(car)){
+        }else{
+            
+        }
+        console.log(regex[i]);
+    }
+    /*
+    const estadoInicial = new Estado('s0');
+    const afn = new AFN(estadoInicial);
+    const estado1 = new Estado('s1');
+    const estado7 = new Estado('s7');
+    const estado1Tr1 = estadoInicial.crearTransicionSobreMi( estado1, '' );
+    const estado1Tr2 = estadoInicial.crearTransicionSobreMi( estado7, '' );
+    */
+})()
