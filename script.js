a2456e70416cbb311ea46c990edda3da8397ae52
David Nuñez <nundavid99@gmail.com>
2021-04-19 19:57:24 -0400

    Matrix implementation

--- script.js
+++ script.js
@@ -21,25 +21,40 @@ function hola(){
 document.getElementById('regexInputButton').addEventListener('click', hola);
 
 (() => {
-    const regex = "(a|b)*abb";
-    
-    const estadoInicial = new Estado('s0');
-    const afn = new AFN(estadoInicial);
-
-    for( let i = 0; i < regex.length; i++ ){
-        const car = regex[i];
-        if("|()*+".contains(car)){
-        }else{
-            
+    const printMat = mat =>{
+        let toPrint = '';
+        for( let i = 0; i < mat.length; i++ ){
+            for( let j = 0; j < mat[j].length; j++ ){
+                const el = mat[i][j];
+                if(el)
+                    toPrint = `${toPrint}\t${el}`;
+                else
+                    toPrint = `${toPrint}\t_`;
+            }
+            toPrint = `${toPrint}\n`;
         }
-        console.log(regex[i]);
+        console.log(toPrint);
     }
-    /*
-    const estadoInicial = new Estado('s0');
-    const afn = new AFN(estadoInicial);
-    const estado1 = new Estado('s1');
-    const estado7 = new Estado('s7');
-    const estado1Tr1 = estadoInicial.crearTransicionSobreMi( estado1, '' );
-    const estado1Tr2 = estadoInicial.crearTransicionSobreMi( estado7, '' );
-    */
+    const START = "__START__";
+    const END = "__END__";
+    const alphabetA = "baaa";
+    const alphabet = Array.from(
+        new Set(
+            alphabetA.split('').sort()
+        )
+    ).join('');
+    console.log('Alphabet: ' + alphabet);
+    let mat = [[]];
+    mat[0] = new Array(alphabet.length);
+    const input = "abba";
+    let currentIndexMatrix = 0;
+    for( let i = 0; i < input.length; i++ ){
+        const car = input[i];
+        const colN = alphabet.indexOf(car);
+        mat[currentIndexMatrix][colN] = currentIndexMatrix + 1;
+        currentIndexMatrix++;
+        if( i != input.length - 1 )
+            mat[currentIndexMatrix] = new Array(alphabet.length);
+    }
+    printMat(mat);
 })()
