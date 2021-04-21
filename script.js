ce429a09ccc6e16391acb7e7e019f7ac3fc1b7dd
David Nuñez <nundavid99@gmail.com>
2021-04-21 07:15:42 -0400

    Clean and OR working

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ document.getElementById('regexInputButton').addEventListener('click', hola);
     const printMat = mat =>{
         let toPrint = '';
         for( let i = 0; i < mat.length; i++ ){
+            toPrint = `${toPrint} State ${i+1}:\t`;
             for( let j = 0; j < mat[i].length; j++ ){
                 const el = mat[i][j];
                 if(el)
@@ -33,6 +34,7 @@ document.getElementById('regexInputButton').addEventListener('click', hola);
             }
             toPrint = `${toPrint}\n`;
         }
+        toPrint = `${toPrint} State ${mat.length+1}:\t\t  FINAL`;
         console.log(toPrint);
     }
     const alphabetA = "baaa";
@@ -44,12 +46,30 @@ document.getElementById('regexInputButton').addEventListener('click', hola);
     console.log('Alphabet: ' + alphabet);
     let mat = [[]];
     mat[0] = new Array(alphabet.length);
-    const input = "ab*";
+    const input = "ab|ba";
     
     const recursiveGetMatrix = (regex, currStateNumber) => {
         const orPos = regex.indexOf('|');
         const cleanPos = regex.indexOf('*');
         if(orPos !== -1){
+            const sMat = recursiveGetMatrix(regex.substring(0, orPos), currStateNumber +1); //load the S mat with the things left to the '|'
+            const sMatStatesQuant = sMat.length;
+            let lastStateNumberS = sMatStatesQuant + currStateNumber;
+            const tMat = recursiveGetMatrix(regex.substring(orPos + 1), lastStateNumberS +2);//loat the T mat with the things right to the '|'
+            const tMatStatesQuant = tMat.length;
+            let lastStateNumber = lastStateNumberS + tMatStatesQuant + 3;
+            
+            let lastSState = new Array(alphabet.length+1); //new END for S
+            lastSState[alphabet.length] = [lastStateNumber]; //Make THE NEW end of S point to new last state
+            sMat.push(lastSState);//push the new END
+            
+            let lastTState = new Array(alphabet.length+1);//new END fot T
+            lastTState[alphabet.length] = [lastStateNumber] //Make the NEW end of T point to new last state
+            tMat.push(lastTState); //Push the new END into T
+
+            let newState = new Array(alphabet.length+1);
+            newState[alphabet.length] = [currStateNumber+1, lastStateNumberS+2];//make the new state point to the S and T mats
+            return [newState].concat(sMat).concat(tMat);
 
         }else if(cleanPos !== -1){
             const sMat = recursiveGetMatrix( regex.substring(0, cleanPos), currStateNumber + 1);//load the matrix with the nexts states
@@ -59,15 +79,13 @@ document.getElementById('regexInputButton').addEventListener('click', hola);
             sMat[statesQuant-1][alphabet.length].push(currStateNumber+1);//The end of s has to the start of s
             
             let newState = new Array(alphabet.length + 1);//create new state that will point to s
-            console.log(newState);
             newState[alphabet.length] = [currStateNumber + 1, lastStateNumber+1];//The start has to point to the end
-            console.log(newState);
             return [newState].concat(sMat);
         }else if(regex.length === 1){
-            let currRow = new Array(alphabet.length + 1);
-            currRow[alphabet.length] = [];
-            const colN = alphabet.indexOf(regex);
-            currRow[colN] = currStateNumber + 1;
+            let currRow = new Array(alphabet.length + 1);//new array for the new state
+            currRow[alphabet.length] = [];//the last element of the array is another array with VACIO pointers
+            const colN = alphabet.indexOf(regex);//The column to load is determined by the position of the character in the alphabet
+            currRow[colN] = currStateNumber + 1;//The end should point to the next state
             return [currRow];
         }else if(regex.length > 1){
             return recursiveGetMatrix(regex[0], currStateNumber).concat(
@@ -75,5 +93,5 @@ document.getElementById('regexInputButton').addEventListener('click', hola);
             );
         }
     }
-    printMat(recursiveGetMatrix(input, 0));
+    printMat(recursiveGetMatrix(input, 1));
 })()
