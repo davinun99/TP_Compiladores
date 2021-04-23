d34d3ffb02038fceb9c7dd478f08b4b4b741bdc3
David <david.nunez@oktana.io>
2021-04-23 11:03:26 -0400

    New helper file and finished handling parenthesis

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+import Thompson from './utils/Thompson.js';
 //document.getElementById('regexInputButton').addEventListener('click', hola);
 
 (() => {
-    const input = "a(b*)"; //Test input
+    const input = "(ab)*b"; //Test input
     const alphabetA = "baaa"; //Test alphabet
     const printMat = mat =>{// This just print the matrix
         let toPrint = '';
@@ -24,69 +25,9 @@
             alphabetA.split('').sort()
         )
     ).join('');//Extract the unique characters and sort them
+    console.log('Input: ' + input);
     console.log('Alphabet: ' + alphabet);
-    const recursiveGetMatrix = (regex, currStateNumber) => {//Function to consume regex and return the Matrix
-        const orPos = regex.indexOf('|');
-        const cleanPos = regex.indexOf('*');
-        const openPar = regex.indexOf('(');
-        const closePar = regex.lastIndexOf(')');
-
-        if(closePar !== -1 && openPar !== -1){
-            const newRegex = regex.substring(openPar+1, closePar);
-            if(closePar === regex.length-1){
-                return recursiveGetMatrix(newRegex, currStateNumber);
-            }else if(cleanPos === closePar + 1){
-                //clean the newRegex
-            }else if(
-                regex[orPos+1] === '(' &&
-                regex[orPos-1] === ')'
-            ){
-                //Or in between
-            }
-            
-        }else if(orPos !== -1){
-            const sMat = recursiveGetMatrix(regex.substring(0, orPos), currStateNumber +1); //load the S mat with the things left to the '|'
-            const sMatStatesQuant = sMat.length;
-            let lastStateNumberS = sMatStatesQuant + currStateNumber;
-            const tMat = recursiveGetMatrix(regex.substring(orPos + 1), lastStateNumberS +2);//loat the T mat with the things right to the '|'
-            const tMatStatesQuant = tMat.length;
-            let lastStateNumber = lastStateNumberS + tMatStatesQuant + 3;
-            
-            let lastSState = new Array(alphabet.length+1); //new END for S
-            lastSState[alphabet.length] = [lastStateNumber]; //Make THE NEW end of S point to new last state
-            sMat.push(lastSState);//push the new END
-            
-            let lastTState = new Array(alphabet.length+1);//new END fot T
-            lastTState[alphabet.length] = [lastStateNumber] //Make the NEW end of T point to new last state
-            tMat.push(lastTState); //Push the new END into T
-
-            let newState = new Array(alphabet.length+1);
-            newState[alphabet.length] = [currStateNumber+1, lastStateNumberS+2];//make the new state point to the S and T mats
-            return [newState].concat(sMat).concat(tMat);
-
-        }else if(cleanPos !== -1){
-            const sMat = recursiveGetMatrix( regex.substring(0, cleanPos), currStateNumber + 1);//load the matrix with the nexts states
-            const statesQuant = sMat.length;
-            let lastStateNumber = statesQuant + currStateNumber + 2;
-
-            let lastSState = new Array(alphabet.length+1); //new END for S
-            lastSState[alphabet.length] = [lastStateNumber, currStateNumber+1]; //The end of s has to point to the new end and start of S
-            sMat.push(lastSState);//push the new END
-
-            let newState = new Array(alphabet.length + 1);//create new state that will point to s
-            newState[alphabet.length] = [currStateNumber + 1, lastStateNumber];//The start has to point to the end
-            return [newState].concat(sMat);
-        }else if(regex.length === 1){
-            let currRow = new Array(alphabet.length + 1);//new array for the new state
-            currRow[alphabet.length] = [];//the last element of the array is another array with VACIO pointers
-            const colN = alphabet.indexOf(regex);//The column to load is determined by the position of the character in the alphabet
-            currRow[colN] = currStateNumber + 1;//The end should point to the next state
-            return [currRow];
-        }else if(regex.length > 1){
-            return recursiveGetMatrix(regex[0], currStateNumber).concat(
-                recursiveGetMatrix(regex.substring(1), currStateNumber+1)
-            );
-        }
-    }
-    printMat(recursiveGetMatrix(input, 1));//Print the generated matrix
+    const mt = Thompson(alphabet, input);
+    printMat(mt);
+    document.getElementById('result').innerHTML = result;
 })()
---
+++ utils/Thompson.js
@@ -0,0 +1,105 @@
+
+
+function Thompson(alphabet, input){
+    const doClean = (regex, cleanPos, currStateNumber) => {
+        const sMat = recursiveGetMatrix( regex.substring(0, cleanPos), currStateNumber + 1);//load the matrix with the nexts states
+        const statesQuant = sMat.length;
+        let lastStateNumber = statesQuant + currStateNumber + 2;
+
+        let lastSState = new Array(alphabet.length+1); //new END for S
+        lastSState[alphabet.length] = [lastStateNumber, currStateNumber+1]; //The end of s has to point to the new end and start of S
+        sMat.push(lastSState);//push the new END
+
+        let newState = new Array(alphabet.length + 1);//create new state that will point to s
+        newState[alphabet.length] = [currStateNumber + 1, lastStateNumber];//The start has to point to the end
+        return [newState].concat(sMat);
+    }
+    const doOr = (regex, orPos, currStateNumber) =>{
+        const sMat = recursiveGetMatrix(regex.substring(0, orPos), currStateNumber +1); //load the S mat with the things left to the '|'
+        const sMatStatesQuant = sMat.length;
+        let lastStateNumberS = sMatStatesQuant + currStateNumber;
+        const tMat = recursiveGetMatrix(regex.substring(orPos + 1), lastStateNumberS +2);//loat the T mat with the things right to the '|'
+        const tMatStatesQuant = tMat.length;
+        let lastStateNumber = lastStateNumberS + tMatStatesQuant + 3;
+        
+        let lastSState = new Array(alphabet.length+1); //new END for S
+        lastSState[alphabet.length] = [lastStateNumber]; //Make THE NEW end of S point to new last state
+        sMat.push(lastSState);//push the new END
+        
+        let lastTState = new Array(alphabet.length+1);//new END fot T
+        lastTState[alphabet.length] = [lastStateNumber] //Make the NEW end of T point to new last state
+        tMat.push(lastTState); //Push the new END into T
+
+        let newState = new Array(alphabet.length+1);
+        newState[alphabet.length] = [currStateNumber+1, lastStateNumberS+2];//make the new state point to the S and T mats
+        return [newState].concat(sMat).concat(tMat);
+    }
+    const doSequence = (regex, currStateNumber)=>{
+        let currRow = new Array(alphabet.length + 1);//new array for the new state
+        currRow[alphabet.length] = [];//the last element of the array is another array with VACIO pointers
+        const colN = alphabet.indexOf(regex);//The column to load is determined by the position of the character in the alphabet
+        currRow[colN] = currStateNumber + 1;//The end should point to the next state
+        return [currRow];
+    }
+    const getParClosing = (regex, openParPos)=> {
+        let checker = 1;//when checker is 0 again we can return
+        for (let i = openParPos+1; i < regex.length; i++) {//go around regex starting from openParPos+1
+            const element = regex[i];
+            if(element === '(') checker++;
+            if(element === ')') checker--;
+            if(!checker){//when checker is 0 again we can return the position
+                return i;
+            }
+        }
+    }
+    const recursiveGetMatrix = (regex, currStateNumber) => {//Function to consume regex and return the Matrix
+        if(!regex){
+            return null;
+        }
+        const orPos = regex.indexOf('|');
+        const cleanPos = regex.indexOf('*');
+        const openPar = regex.indexOf('(');
+        const closePar = getParClosing(regex, openPar);
+        
+        if(closePar !== -1 && openPar !== -1){
+            const newRegex = regex.substring(openPar+1, closePar);
+            if(closePar === regex.length-1){
+                return recursiveGetMatrix(newRegex, currStateNumber);
+            }else if(closePar + 1 === orPos || openPar + 1 === orPos){//Or is already at max priority so just removing the parenthesis if their redundant
+                const regexWithoutPar = regex.splice(0,openPar) + regex.splice(openPar+1, closePar) + regex.substring(closePar+1); //remove parenthesis
+                return recursiveGetMatrix(regexWithoutPar, currStateNumber);
+            }
+            else if(cleanPos === closePar + 1){
+                let cleanedMat = doClean(newRegex, cleanPos, currStateNumber); //Clean the regex
+                const regexAfterC = regex.substring(cleanPos + 1); //substr the regex after 
+                if(regexAfterC ){//If next characters exist
+                    const lastState = cleanedMat.length + currStateNumber;
+                    const nextMat = recursiveGetMatrix(regexAfterC, lastState);
+                    cleanedMat = cleanedMat.concat(nextMat);
+                }
+                return cleanedMat;
+            }else if(
+                regex[orPos+1] === '(' &&
+                regex[orPos-1] === ')'
+            ){
+                //Or in between
+            }
+            
+        }else if(orPos !== -1){
+            return doOr(regex, orPos, currStateNumber);
+
+        }else if(cleanPos !== -1){
+            return doClean(regex, cleanPos, currStateNumber);
+
+        }else if(regex.length === 1){
+            return doSequence(regex, currStateNumber);
+
+        }else if(regex.length > 1){
+            return recursiveGetMatrix(regex[0], currStateNumber).concat(
+                recursiveGetMatrix(regex.substring(1), currStateNumber+1)
+            );
+        }
+    }
+    return (recursiveGetMatrix(input, 1));
+}
+export default Thompson;
