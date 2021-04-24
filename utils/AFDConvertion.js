802517cdffc4b75065cabe0f86339605d50ebf09
David Nuñez <nundavid99@gmail.com>
2021-04-23 20:58:02 -0400

    closure finished and started structure

--- utils/AFDConvertion.js
+++ utils/AFDConvertion.js
@@ -1,33 +1,46 @@
 
-class DEstadosContainer{
-    estados = [];
+class DEstatesContainer{
+    statesSet = [];
     constructor(){
         
     }
     hasSet(set){
+        this.statesSet.forEach(states =>{
+            states.forEach((state, index) =>{
+                
+            })
+        })
         return false;
     }
     get(index){
-        return this.estados[index];
+        return this.statesSet[index];
     }
     add(set){
-        this.estados = this.estados.concat(set);
+        this.statesSet = this.statesSet.concat(set);
     }
 }
 
 export default function AFDConvertion(alphabet, matrix){
     
     const closeClosure = (stateNumber, character) => {
+        const indexOfChar = alphabet.indexOf(character);
         if(matrix.length === stateNumber){
             return [];
         }else if(character === ''){//IF VACIO CHARACTER
-            const lastIndex = matrix[stateNumber].length -1;
-            if(matrix[stateNumber][lastIndex] && matrix[stateNumber][lastIndex].length){
-                let nextStates = [];
-                matrix[stateNumber][lastIndex].forEach(state => {
-                    nextStates = nextStates.concat(closeClosure(state, character));
+            const lastIndex = matrix[stateNumber].length -1;//VACIOS ARE SAVED IN THE LAST INDEX OF THE ROW
+            if(matrix[stateNumber][lastIndex] && matrix[stateNumber][lastIndex].length){ //IF THE LAST INDEX OF THE ROW IS NOT EMPTY
+                let nextStates = [];//VECTOR TO LOAD NEXT STATES TO VISIT
+                matrix[stateNumber][lastIndex].forEach(state => {//FOR EACH STATE PENDING OF VISIT
+                    nextStates = nextStates.concat(closeClosure(state, character));//LOAD THE NEXT STATES TO VISIT
                 });
-                return matrix[stateNumber][lastIndex].concat(nextStates);
+                return matrix[stateNumber][lastIndex].concat(nextStates); //RETURN ALL THE STATES TO VISIT OF THIS ROW + THE STATES VISITED ABOVE
+            }else{
+                return [];//IF NO STATES TO VISIT RETURN []
+            }
+        }else if(indexOfChar !== -1){//IF ALPHABET CHARACTER
+            const nextStates = matrix[stateNumber][indexOfChar];
+            if(nextStates){
+                return [nextStates].concat(closeClosure(nextStates, character));//return the state + the next states possibles
             }else{
                 return [];
             }
@@ -37,7 +50,7 @@ export default function AFDConvertion(alphabet, matrix){
     const dEstates = new DEstadosContainer( );
     let dEstatesCount = 0;
     const dTran = [[]];
-    console.log(closeClosure(0, ''));
+    console.log(closeClosure(2, 'b'));
     return;
     while(true){ //while dEstates has unmarked states
         const tState = dEstates.get(dEstatesCount);
